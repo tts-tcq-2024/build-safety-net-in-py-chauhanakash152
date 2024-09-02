68ebe0f35214e45735891b49533506a25cca0a26
chauhanakash152 <chauhanakash152@gmail.com>
2024-09-03 03:24:28 +0530

    reduce ccn

--- Soundex.py
+++ Soundex.py
@@ -1,5 +1,5 @@
-def get_soundex_code(char):
-    """Returns the Soundex code for a single character."""
+def get_soundex_code(c):
+    c = c.upper()
     mapping = {
         'B': '1', 'F': '1', 'P': '1', 'V': '1',
         'C': '2', 'G': '2', 'J': '2', 'K': '2', 'Q': '2', 'S': '2', 'X': '2', 'Z': '2',
@@ -8,38 +8,34 @@ def get_soundex_code(char):
         'M': '5', 'N': '5',
         'R': '6'
     }
-    return mapping.get(char.upper(), '')
+    return mapping.get(c, '0')  # Default to '0' for non-mapped characters
 
-def encode_name(name):
-    """Encodes the name into Soundex digits, collapsing adjacent same digits and handling separation by vowels."""
-    if not name:
-        return "0000"
-    
-    # Remove irrelevant characters and handle the encoding
-    first_letter = name[0].upper()
-    previous_code = get_soundex_code(first_letter)
-    encoded_digits = [previous_code]
-    
-    for char in name[1:]:
-        code = get_soundex_code(char)
-        if code and code != previous_code:
-            encoded_digits.append(code)
-            previous_code = code
-        if len(encoded_digits) == 4:
-            break
-
-    return first_letter + ''.join(encoded_digits[1:]).ljust(3, '0')
+def validate_rule(code , prev_code):
+    return code != '0' and code != prev_code
 
 def generate_soundex(name):
-    """Generates the Soundex code for a given name."""
-    return encode_name(name)
+    if not name:
+        return ""
+        # Start with the first letter (capitalized)
+    soundex = name[0].upper()
+    prev_code = get_soundex_code(soundex)
 
+    for char in name[1:]:
+        code = get_soundex_code(char)
+        if validate_rule(code, prev_code):
+            soundex += code
+            prev_code = code
+        if len(soundex) == 4:
+            break
+    # Pad with zeros if necessary
+    soundex = soundex.ljust(4, '0')
+    return soundex
 # Test cases
-# print(generate_soundex("Robert"))  # Output should be "R163"
-# print(generate_soundex("Rupert"))  # Output should be "R163"
-# print(generate_soundex("Rubin"))   # Output should be "R150"
-# print(generate_soundex("Ashcraft")) # Output should be "A261"
-# print(generate_soundex("Ashcroft")) # Output should be "A261"
+print(generate_soundex("Robert"))  # Output should be "R163"
+print(generate_soundex("Rupert"))  # Output should be "R163"
+print(generate_soundex("Rubin"))   # Output should be "R150"
+print(generate_soundex("Ashcraft")) # Output should be "A261"
+print(generate_soundex("Ashcroft")) # Output should be "A261"
 # print(generate_soundex("Tymczak"))  # Output should be "T522"
 # print(generate_soundex("Pfister"))  # Output should be "P236"
 # print(generate_soundex("Honeyman"))  # Output should be "H555"
