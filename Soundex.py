efc58ae55296e93daf6111c7775a54d5c26f1ae9
chauhanakash152 <chauhanakash152@gmail.com>
2024-09-02 22:40:54 +0530

    first commit

--- Soundex.py
+++ Soundex.py
@@ -1,5 +1,5 @@
 def get_soundex_code(c):
-    c = c.upper()
+    """Returns the Soundex code for a single character."""
     mapping = {
         'B': '1', 'F': '1', 'P': '1', 'V': '1',
         'C': '2', 'G': '2', 'J': '2', 'K': '2', 'Q': '2', 'S': '2', 'X': '2', 'Z': '2',
@@ -8,26 +8,35 @@ def get_soundex_code(c):
         'M': '5', 'N': '5',
         'R': '6'
     }
-    return mapping.get(c, '0')  # Default to '0' for non-mapped characters
+    return mapping.get(c.upper(), '0')  # Default to '0' for non-mapped characters
 
+def get_first_letter(name):
+    """Returns the first letter of the name in uppercase."""
+    return name[0].upper() if name else ""
+
+def filter_and_map_soundex(name):
+    """Returns a list of Soundex codes for the characters in the name, skipping repeated codes."""
+    codes = []
+    prev_code = ''
+    for char in name:
+        code = get_soundex_code(char)
+        if code != '0' and code != prev_code:
+            codes.append(code)
+            prev_code = code
+    return codes
+
+def pad_soundex(soundex):
+    """Pads the Soundex code with zeros to ensure it is 4 characters long."""
+    return soundex.ljust(4, '0')
 
 def generate_soundex(name):
+    """Generates the Soundex code for a given name."""
     if not name:
         return ""
 
-    # Start with the first letter (capitalized)
-    soundex = name[0].upper()
-    prev_code = get_soundex_code(soundex)
+    first_letter = get_first_letter(name)
+    soundex_body = ''.join(filter_and_map_soundex(name[1:]))[:3]  # Take only the first 3 mapped codes
+    soundex = first_letter + soundex_body
 
-    for char in name[1:]:
-        code = get_soundex_code(char)
-        if code != '0' and code != prev_code:
-            soundex += code
-            prev_code = code
-        if len(soundex) == 4:
-            break
+    return pad_soundex(soundex)
 
-    # Pad with zeros if necessary
-    soundex = soundex.ljust(4, '0')
-
-    return soundex
