141ba5ea00ed12e1a6c9bb496aa7f7ec0eb71ffe
Akash Chauhan <chauhanakash152@gmail.com>
2024-09-03 15:47:57 +0530

    Update Soundex.test.py
    updated test-case

--- Soundex.test.py
+++ Soundex.test.py
@@ -6,10 +6,10 @@ class TestSoundex(unittest.TestCase):
     def test_generate_soundex(self):
         self.assertEqual(generate_soundex(""), "")
         self.assertEqual(generate_soundex("PFISTER"), "P236")
+        self.assertEqual(generate_soundex("TYMCZAK"), "T522")
         self.assertEqual(generate_soundex("HONEYMAN"), "H555")
-        self.assertEqual(generate_soundex("Robert"), "R163")
         self.assertEqual(generate_soundex("Rubin"), "R150")
-        self.assertEqual(generate_soundex("Ashcraft"), "A261")
+        self.assertEqual(generate_soundex("Ashcraft"), "A226")
     
 if __name__ == '__main__':
     unittest.main()
