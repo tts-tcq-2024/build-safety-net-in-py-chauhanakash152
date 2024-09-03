2c9b0915d7bfe6b3dbf945a2a4991e54d7c4583b
Akash Chauhan <chauhanakash152@gmail.com>
2024-09-03 14:28:00 +0530

    Update Soundex.test.py
    Updated test

--- Soundex.test.py
+++ Soundex.test.py
@@ -3,30 +3,13 @@ from Soundex import generate_soundex
 
 class TestSoundex(unittest.TestCase):
 
-    def test_empty_string(self):
+    def test_generate_soundex(self):
         self.assertEqual(generate_soundex(""), "")
-
-    def test_single_character(self):
-        self.assertEqual(generate_soundex("A"), "A000")
-
-    def test_single_character_non_mapped(self):
-        self.assertEqual(generate_soundex("1"), "1000")  # Non-mapped character should default to '0'
-
-    def test_repeated_characters(self):
-        self.assertEqual(generate_soundex("Sssss"), "S000")
-
-    def test_name_with_digits(self):
-        self.assertEqual(generate_soundex("J0hn"), "J500")
-
-    def test_name_with_special_characters(self):
-        self.assertEqual(generate_soundex("John@Doe"), "J530")
-
-    def test_name_with_various_letters(self):
-        self.assertEqual(generate_soundex("Cameron"), "C565")
-        self.assertEqual(generate_soundex("Smith"), "S530")
-        self.assertEqual(generate_soundex("Khan"), "K530")
-
-
+        self.assertEqual(generate_soundex("PFISTER"), "P236")
+        self.assertEqual(generate_soundex("HONEYMAN"), "H555")
+        self.assertEqual(generate_soundex("Robert"), "R163")
+        self.assertEqual(generate_soundex("Rubin"), "R150")
+        self.assertEqual(generate_soundex("Ashcraft"), "A261")
     
 if __name__ == '__main__':
     unittest.main()
